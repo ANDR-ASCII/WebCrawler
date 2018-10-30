a1f554d5dc08781c333ebf893e7ee849a600711f
Andrey Pugachev <pugachev.andrey.work@gmail.com>
2018-10-30 20:46:58 +0300

    Project initialization

---
+++ terminal/src/main.cpp
@@ -0,0 +1,30 @@
+#include "application.h"
+#include "custom_uri_channel.h"
+
+int main(int argc, char *argv[])
+{
+	using namespace SeoSpider;
+
+	if (argc > 1)
+	{
+		const char* arg = argv[1];
+		const QByteArray argStr = QByteArray::fromRawData(arg, static_cast<int>(strlen(arg)));
+		if (argStr.startsWith("seospider:"))
+		{
+			if (CrawlerEngine::CustomUrlChannel::writeUri(argStr))
+			{
+				return 0;
+
+			}
+			// application is not started or something went wrong
+			// TODO: start application with params
+			return 1;
+		}
+
+	}
+
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf8"));
+
+	Application app(argc, argv);
+	return app.exec();
+}
